d6ae1edc913629318d9d2b4043e22775bfd974d6
召 程 <542153354@qq.com>
2019-10-12 10:44:38 +0800

    添加项目文件。

---
+++ CSharp8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp8
+{
+    class Program
+    {
+        public static List<Student> People { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var names = GetEnrollees().ToList();
+            Console.ReadLine();
+        }
+
+        static IEnumerable<string> GetEnrollees()
+        {
+            People = new List<Student>
+            {
+                new Student { Name = "test1", Graduated = true },
+                new Student { Name = "test2", Graduated = false },
+                new Student { Name = "test3", Graduated = false },
+                new Student { Name = "test4", Graduated = false },
+            };
+            foreach (var p in People)
+            {
+                if (p is Student { Graduated: false, Name: string name })
+                {
+                    yield return name;
+                }
+            }
+        }
+    }
+
+    public class Student
+    {
+        public bool Graduated { get; set; }
+        public string Name { get; set; }
+    }
+}
